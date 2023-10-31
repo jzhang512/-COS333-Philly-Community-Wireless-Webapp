c2ce30fa2c3704f8d4b79729ee2a0bef51d5f222
Samuel Sanft <sanft@princeton.edu>
2023-10-31 16:13:28 -0400

    add get request for '/pin'

--- pcw.py
+++ pcw.py
@@ -10,14 +10,29 @@ app=flask.Flask(__name__)
 def index():
     try:
         pins = database.get_pins()
+        tags = database.get_tags()
     except Exception as ex:
         print(ex)
         html_code = flask.render_template('error.html')
-        response = flask.make_response(html_code)
-
-        return response
+        return flask.make_response(html_code)
     
-    html_code = flask.render_template('index.html', pins=pins)
+    html_code = flask.render_template('index.html', pins=pins, tags=tags)
     response = flask.make_response(html_code)
 
     return response
+
+
+@app.route('/pin', methods=['GET'])
+def pin():
+    pin = flask.request.args.get("id")
+    try:
+        pin = int(pin)
+    except ValueError as ex:
+        return flask.jsonify("Invalid Arg, not an int")
+    try:
+        reviews = database.get_reviews(pin)
+    except Exception as ex:
+        print(ex)
+        return flask.jsonify("Database Error")
+
+    return flask.jsonify(reviews)
