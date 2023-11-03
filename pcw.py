5907c8322d4ef74f4a0df85da0699c88e38d0705
Samuel Sanft <sanft@princeton.edu>
2023-11-02 20:17:35 -0400

    Update routes for api

--- pcw.py
+++ pcw.py
@@ -8,27 +8,31 @@ app=flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    try:
-        pins = database_req.get_pins()
-        tags = database_req.get_tags()
-    except Exception as ex:
-        print(ex)
-        html_code = flask.render_template('error.html')
-        return flask.make_response(html_code)
-    
-    html_code = flask.render_template('index.html', pins=pins, tags=tags)
+    html_code = flask.render_template('index.html')
     response = flask.make_response(html_code)
 
     return response
 
 
-@app.route('/pin', methods=['GET'])
+@app.route('/api/hotspots', methods=['GET'])
+def hotspots():
+    try:
+        pins = database_req.get_pins
+    except Exception as ex:
+        print(ex)
+        return flask.jsonify("Database Error")
+    
+    return flask.jsonify(pins)
+
+
+@app.route('/api/reviews', methods=['GET'])
 def pin():
     pin = flask.request.args.get("id")
     try:
         pin = int(pin)
     except ValueError as ex:
         return flask.jsonify("Invalid Arg, not an int")
+    
     try:
         reviews = database_req.get_reviews(pin)
     except Exception as ex:
