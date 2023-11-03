63a7d3e196542560b9a99df5c7e971bef304cf3c
Samuel Sanft <sanft@princeton.edu>
2023-11-03 02:44:40 -0400

    update popup template and flask code

--- pcw.py
+++ pcw.py
@@ -6,7 +6,7 @@ import flask
 import database_req
 
 
-app = flask.Flask(__name__, static_folder='images', template_folder='.')
+app = flask.Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
@@ -16,37 +16,6 @@ def index():
     html_code = flask.render_template('index.html')
     # print(html_code)
     response = flask.make_response(html_code)
-    return response
-
-
-@app.route('/info', methods=['GET'])
-def info():
-    try:
-        curr_id = flask.request.args.get("id")
-        index = int(curr_id)-1
-        pin = database_req.get_pins()
-        # print(pin)
-        tags = database_req.get_tags()
-        reviews = database_req.get_reviews(int(curr_id))
-        # print(reviews)
-        html_code = flask.render_template(
-            'templates/popup.html', pins=[pin[index]], tags=[tags[index]], reviews=reviews)
-
-        return html_code
-
-    except ValueError as ex:
-        return flask.jsonify("Invalid Arg, not an int")
-    except Exception as ex:
-        print(ex)
-        return flask.jsonify("Database Error")
-
-
-@app.route('/get_all', methods=['GET'])
-def get_all():
-
-    html_code = flask.render_template('index.html')
-    response = flask.make_response(html_code)
-
     return response
 
 
@@ -77,3 +46,9 @@ def pin():
         return flask.jsonify("Database Error")
 
     return flask.jsonify(reviews)
+
+
+@app.route('/api/popup', methods=['GET'])
+def popup():
+    html_code = flask.render_template('popup.html')
+    return flask.make_response(html_code) 
