ced24110f06b8f52e44df8315d4f1e7e9d37f066
John Van Horn <jv8294@princeton.edu>
2023-11-02 21:54:01 -0400

    connected front and backend

--- pcw.py
+++ pcw.py
@@ -3,20 +3,53 @@ import flask
 import database_req
 
 
-app=flask.Flask(__name__)
+app = flask.Flask(__name__, static_folder='images', template_folder='.')
 
 
 @app.route('/', methods=['GET'])
 def index():
+    # build the html code
+    html_code = flask.render_template('index.html')
+    # print(html_code)
+    response = flask.make_response(html_code)
+    return response
+
+
+@app.route('/info', methods=['GET'])
+def info():
+    try:
+        curr_id = flask.request.args.get("id")
+        index = int(curr_id)-1
+        pin = database_req.get_pins()
+        # print(pin)
+        tags = database_req.get_tags()
+        reviews = database_req.get_reviews(int(curr_id))
+        # print(reviews)
+        html_code = flask.render_template(
+            'templates/popup.html', pins=[pin[index]], tags=[tags[index]], reviews=reviews)
+
+        return html_code
+
+    except ValueError as ex:
+        return flask.jsonify("Invalid Arg, not an int")
+    except Exception as ex:
+        print(ex)
+        return flask.jsonify("Database Error")
+
+
+@app.route('/get_all', methods=['GET'])
+def get_all():
     try:
         pins = database_req.get_pins()
         tags = database_req.get_tags()
     except Exception as ex:
         print(ex)
-        html_code = flask.render_template('error.html')
+        html_code = flask.render_template('templates/error.html')
         return flask.make_response(html_code)
-    
-    html_code = flask.render_template('index.html', pins=pins, tags=tags)
+
+    # DEPRECATED FOR NOW
+    # html_code = flask.render_template(
+    #     'templates/popup.html', pins=pins, tags=tags)
     response = flask.make_response(html_code)
 
     return response
