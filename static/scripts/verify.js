f612f0e5cda20f2ca995c02cad8de1268161a8ab
John Van Horn <jv8294@princeton.edu>
2023-11-09 17:37:40 -0500

    added admin functionality

---
+++ static/scripts/verify.js
@@ -0,0 +1,77 @@
+function fillReviews(reviews) {
+    const reviewsDiv = document.getElementById('review-list');
+
+    if (reviews.length == 0) {
+        reviewsDiv.textContent = "No unverified reviews.";
+    }
+
+    for (const review in reviews) {
+        reviewsDiv.innerHTML = "";
+        const newReview = document.createElement("p");
+        newReview.textContent = "Review " + review['pin_id'];
+        reviewsDiv.appendChild(newReview);
+    }
+
+}
+
+function displayActive(review) {
+    const activeDiv = document.getElementById("review-list");
+    activeDiv.innerHTML = '';
+    if (review == null) {
+        const cardText = document.createElement("p");
+        cardText.textContent = "No review selected";
+        activeDiv.appendChild(cardText);
+    }
+
+    else {
+
+        const cardDiv = document.createElement("div");
+        cardDiv.classList.add("card");
+        const cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+        const cardTitle = document.createElement("h5")
+        cardTitle.classList.add("card-title");
+        const title = document.createTextNode("Review " + i);
+        const starsPara = document.createElement("p");
+        const stars = document.createTextNode("Stars: " + review['stars']);
+        const timePara = document.createElement("p");
+        const time = document.createTextNode(review['time']);
+        const bodyPara = document.createElement("p");
+        const body = document.createTextNode(review['text']);
+        const verify = document.createElement("button");
+        verify.id = 'verify';
+        const deny = document.createElement("button");
+        deny.id = 'deny';
+
+
+        cardTitle.appendChild(title);
+        starsPara.appendChild(stars);
+        timePara.appendChild(time);
+        bodyPara.appendChild(body);
+
+        cardBody.appendChild(cardTitle);
+        cardBody.appendChild(starsPara);
+        cardBody.appendChild(timePara);
+        cardBody.appendChild(bodyPara);
+        cardBody.appendChild(verify);
+        cardBody.appendChild(deny);
+
+        cardDiv.appendChild(cardBody);
+        activeDiv.appendChild(cardDiv);
+
+        verify.addEventListener('click', function () {
+            manageReview(true);
+        });
+        deny.addEventListener('click', function () {
+            manageReview(false);
+        });
+    }
+}
+
+async function manageReview(isVerify) {
+    if (isVerify) {
+        // Load hotspots and popup html
+        const response = await fetch("/api/hotspots");
+        const hotspots = await response.json();
+    }
+}
