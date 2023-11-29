53766bb8341de46d85de39f7bdf31d7c610d619e
James Zhang <james.zhang@princeton.edu>
2023-11-28 23:20:44 -0500

    added filter sidebar

--- static/scripts/filter_sidebar.js
+++ static/scripts/filter_sidebar.js
@@ -34,9 +34,6 @@ $(document).ready(async function() {
     tags.sort((a, b) => a['tag_name'].localeCompare(b['tag_name']))
     console.log
 
-    let categories = ['Cost', 'Privacy', 'Password', 'Amenities', 
-                      'Establishment', 'Accessibility']
-
     // $("#sidebarToggle").click(function() {
     //     console.log("toggled")
     //     $("#leftSidebar").toggleClass("left-sidebar-active");
@@ -77,15 +74,41 @@ $(document).ready(async function() {
         updateHotspotsList(hotspots);
     })
 
+    categories = [];
+    tags.forEach((tag) => {
+        let category = tag['category'];
+        if (!categories.includes(category)) {
+            categories.push(category);
+        }
+    });
+
+    categories.sort();
+    categories.forEach((cat) => {
+        $('#filterView').append($('<h6 id=\"' + cat + 'tag\" class = \"tagHeader\">' + cat + '<br></h6>'));
+    });
+
     tags.forEach((tag) => {
         let category = tag['category'];
         let tagName = tag['tag_name'];
         let tagId = tag['tag_id'];
-        $('#tagContainer' + category).append(
-            $('<label class="form-check form-check-inline"></label>').append(
-                $('<input name=btn' + category + ' id=' + tagId +' type="radio" ></input>'),
-                $('<span> </span>').text(tagName)
-            )
+        $('#' + category + "tag").append(
+            $('<input type=\"checkbox\" class=\"btn-check\" id=\"btn-check' + tagId + '\" autocomplete=\"off\">'+
+            '<label class=\"btn btn-outline-secondary col-12\" for=\"btn-check' + tagId + '\">' + tagName + '</label></br>')
+        //    $('<br><input type=\"checkbox\" class=\"btn-check tag\" id=\"btn-check' + tagId + '\" autocomplete=\"off\">'+
+        //    '<br><label class=\"btn btn-outline-secondary\" for=\"btn-check' + tagId + '\">' + tagName + '</label>')
         );
-    });
+    }
+    );
+
+    // tags.forEach((tag) => {
+    //     let category = tag['category'];
+    //     let tagName = tag['tag_name'];
+    //     let tagId = tag['tag_id'];
+    //     $('#' + category + "tag").append(
+    //         $('<label class="form-check form-check-inline"></label>').append(
+    //             $('<input name=btn' + category + ' id=' + tagId +' type="radio" ></input>'),
+    //             $('<span> </span>').text(tagName)
+    //         )
+    //     );
+    // });
 });
