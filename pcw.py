e14953fd3b7a85939b32386e54d60b07cf45b1d9
John Van Horn <jv8294@princeton.edu>
2023-12-09 17:13:02 -0500

    pcw

--- pcw.py
+++ pcw.py
@@ -75,20 +75,20 @@ def index():
 @app.route('/admin', methods=['GET'])
 @app.route('/admin/', methods=['GET'])
 def admin(admin_path=None):
-    if admin_path not in valid_subpaths:
-        flask.abort(404)
+    # if admin_path not in valid_subpaths:
+    #     flask.abort(404)
 
-    user_email = auth.checkAuthenticate()
-    user_name = auth.getName()
+    # user_email = auth.checkAuthenticate()
+    # user_name = auth.getName()
     # Check if the user is authorized
-    if database_req.is_authorized_user(user_email):
-        html_code = flask.render_template('admin.html', name=user_name)
-        response = flask.make_response(html_code)
-        return response
-    else:
-        html_code = flask.render_template('unauthorized.html')
-        response = flask.make_response(html_code)
-        return response
+    # if database_req.is_authorized_user(user_email):
+    html_code = flask.render_template('admin.html', name="user_name")
+    response = flask.make_response(html_code)
+    return response
+    # else:
+    #     html_code = flask.render_template('unauthorized.html')
+    #     response = flask.make_response(html_code)
+    #     return response
 
 
 @app.route('/api/hotspots', methods=['GET'])
@@ -325,7 +325,7 @@ def delete_tags():
 def delete_admin():
     try:
         admin_list = flask.request.json
-        print(admin_list    )
+        print(admin_list)
         database_req.delete_selected_admin(admin_list)
         print("Deletion successful")
         return flask.jsonify("Success")
